9a73a98c9126c4ae363a85a20ac816bdf646c8cf
Tomoshige Shimomura <ii00zero1230@gmail.com>
2017-01-10 00:02:41 +0900

    setup.pyを作成

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def main():
+    description = 'patch_maker package'
+
+    setup(
+        name='patch_maker',
+        version='0.0.1',
+        author='shimtom',
+        author_email='ii00zero1230@gmail.com',
+        url='',
+        description=description,
+        long_description=description,
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=[],
+        tests_require=[],
+        setup_requires=[],
+    )
+
+
+if __name__ == '__main__':
+    main()
