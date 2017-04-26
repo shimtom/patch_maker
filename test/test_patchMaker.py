6b1084759f985ce4ee280ace8e2e42f1f18cf7b0
Tomoshige Shimomura <ii00zero1230@gmail.com>
2017-04-26 14:13:02 +0900

    patch_maker.generate_patches()をテストするメソッドを作成. パッチのサイズ、インターバル、パディング方法のテストは記述済み. ただし、ピクセルごとの値確認は行っていない. また、コードを見やすくするため、変数名を変更した.

--- test/test_patchMaker.py
+++ test/test_patchMaker.py
@@ -20,76 +20,180 @@ class TestPatchMaker(TestCase):
 
         # 成功を確認
         # 中央, 左上, 左下, 右上, 右下, 左中央, 右中央, 下中央, 上中央
-        test_points = [(10, 10), (0, 0), (0, 20), (20, 0),
-                       (20, 20), (0, 10), (20, 10), (10, 20), (0, 10)]
+        points = [(10, 10), (0, 0), (0, 20), (20, 0), (20, 20),
+                  (0, 10), (20, 10), (10, 20), (0, 10)]
         # 幅 = 高さ
-        test_sizes = [(0, 0), (10, 10), (20, 20), (25, 25),
-                      (30, 30), (35, 35), (40, 40)]
+        sizes = [(0, 0), (10, 10), (20, 20), (25, 25),
+                 (30, 30), (35, 35), (40, 40)]
         # 幅 < 高さ
-        test_sizes += [(0, 5), (5, 10), (15, 20), (20, 25),
-                       (25, 30), (30, 35), (35, 40)]
+        sizes += [(0, 5), (5, 10), (15, 20), (20, 25),
+                  (25, 30), (30, 35), (35, 40)]
         # 幅 > 高さ
-        test_sizes += [(5, 0), (10, 5), (20, 15), (25, 20),
-                       (30, 20), (35, 30), (40, 35)]
+        sizes += [(5, 0), (10, 5), (20, 15), (25, 20),
+                  (30, 20), (35, 30), (40, 35)]
 
-        paddings = ['MIRROR', 'SAME', 'VALID']
+        test_paddings = ['MIRROR', 'SAME', 'VALID']
 
-        for test_point in test_points:
-            for test_size in test_sizes:
-                for padding in paddings:
-                    for test_image in self._test_images:
+        for point in points:
+            for size in sizes:
+                for padding in test_paddings:
+                    for image in self._test_images:
                         patch = generate_patch(
-                            self._test_image, test_point, test_size, padding, to_image=True)
+                            image, point, size, padding, to_image=True)
                         array = generate_patch(
-                            self._test_image, test_point, test_size, padding, to_image=False)
-                        self.assertEqual(patch.size, test_size)
-                        self.assertEqual(patch.mode, test_image.mode)
+                            image, point, size, padding, to_image=False)
+                        self.assertEqual(patch.size, size)
+                        self.assertEqual(patch.mode, image.mode)
                         self.assertEqual(patch, Image.fromarray(array))
                         self.assertTrue(np.array_equal(array, np.array(array)))
                         # TODO: ピクセルごとに値の確認
 
         # 失敗を確認
-        failure_test_points = [(-1, -1), (-1, 0), (0, -1)]
-        failure_test_points += [(21, -1), (20, -1), (21, 0)]
-        failure_test_points += [(21, 21), (21, 20), (20, 21)]
-        failure_test_points += [(-1, 21), (-1, 20), (0, 21)]
-        failure_test_points += [(0.1, 0.1), (0.1, 0), (0, 0.1)]
-        failure_test_sizes = [(-1, -1), (41, 41), (-1, 0), (0, -1),
-                              (41, 40), (40, 41), (0.1, 0.1), (0.1, 0), (0, 0.1)]
+        failure_points = [(-1, -1), (-1, 0), (0, -1)]
+        failure_points += [(21, -1), (20, -1), (21, 0)]
+        failure_points += [(21, 21), (21, 20), (20, 21)]
+        failure_points += [(-1, 21), (-1, 20), (0, 21)]
+        failure_points += [(0.1, 0.1), (0.1, 0), (0, 0.1)]
+        failure_sizes = [(-1, -1), (41, 41), (-1, 0), (0, -1),
+                         (41, 40), (40, 41), (0.1, 0.1), (0.1, 0), (0, 0.1)]
         failure_paddings = ['']
 
-        for test_point in failure_test_points:
-            for test_size in test_sizes:
-                for padding in paddings:
-                    for test_image in self._test_images:
-                        with self.assertRaises(ValueError, "invalid coordinate %s" % str(test_point)):
+        for point in failure_points:
+            for size in sizes:
+                for padding in test_paddings:
+                    for image in self._test_images:
+                        with self.assertRaises(ValueError, "invalid coordinate %s" % str(point)):
                             patch = generate_patch(
-                                self._test_image, test_point, test_size, padding, to_image=True)
-                        with self.assertRaises(ValueError, "invalid coordinate %s" % str(test_point)):
+                                image, point, size, padding, to_image=True)
+                        with self.assertRaises(ValueError, "invalid coordinate %s" % str(point)):
                             array = generate_patch(
-                                self._test_image, test_point, test_size, padding, to_image=False)
+                                image, point, size, padding, to_image=False)
 
-        for test_point in test_points:
-            for test_size in failure_test_sizes:
-                for padding in paddings:
-                    for test_image in self._test_images:
-                        with self.assertRaises(ValueError, "invalid size %s" % str(test_size)):
+        for point in points:
+            for size in failure_sizes:
+                for test_padding in test_paddings:
+                    for image in self._test_images:
+                        with self.assertRaises(ValueError, "invalid size %s" % str(size)):
                             patch = generate_patch(
-                                self._test_image, test_point, test_size, padding, to_image=True)
-                        with self.assertRaises(ValueError, "invalid size %s" % str(test_point)):
+                                image, point, size, test_padding, to_image=True)
+                        with self.assertRaises(ValueError, "invalid size %s" % str(point)):
                             array = generate_patch(
-                                self._test_image, test_point, test_size, padding, to_image=False)
+                                image, point, size, test_padding, to_image=False)
 
-        for test_point in test_points:
-            for test_size in test_sizes:
-                for padding in failure_paddings:
-                    for test_image in self._test_images:
-                        with self.assertRaises(ValueError, "invalid padding %s" % padding):
+        for point in points:
+            for size in sizes:
+                for test_padding in failure_paddings:
+                    for image in self._test_images:
+                        with self.assertRaises(ValueError, "invalid padding %s" % test_padding):
                             patch = generate_patch(
-                                self._test_image, test_point, test_size, padding, to_image=True)
-                        with self.assertRaises(ValueError, "invalid size %s" % padding):
+                                image, point, size, test_padding, to_image=True)
+                        with self.assertRaises(ValueError, "invalid size %s" % test_padding):
                             array = generate_patch(
-                                self._test_image, test_point, test_size, padding, to_image=False)
+                                image, point, size, test_padding, to_image=False)
 
     def test_generate_patches(self):
         """画像から指定された条件で、画像から連続してパッチを切り出すメソッドをテストする."""
+        # 幅 = 高さ
+        sizes = [(0, 0), (10, 10), (20, 20), (25, 25),
+                 (30, 30), (35, 35), (40, 40)]
+        # 幅 < 高さ
+        sizes += [(0, 5), (5, 10), (15, 20), (20, 25),
+                  (25, 30), (30, 35), (35, 40)]
+        # 幅 > 高さ
+        sizes += [(5, 0), (10, 5), (20, 15), (25, 20),
+                  (30, 20), (35, 30), (40, 35)]
+        intervals = [0, 1, 20, 40, 1000, 1600]
+        paddings = ['MIRROR', 'SAME', 'VALID']
+
+        for size in sizes:
+            for interval in intervals:
+                for padding in paddings:
+                    for image in self._test_images:
+                        length = image.width * image.height
+                        patches = []
+                        for patch in generate_patches(image, size, interval, padding, to_image=True):
+                            self.assertEqual(image.size, patch.size)
+                            self.assertEqual(image.mode, patch.mode)
+                            patches.append(patch)
+                            # TODO: ピクセルごとに値の確認
+                        self.assertEqual(length, len(patches))
+
+                        count = 0
+                        for i, array in enumerate(generate_patches(image, size, interval, padding, to_image=False)):
+                            self.assertEqual(
+                                patches[i], Image.fromarray(array))
+                            self.assertTrue(np.array_equal(
+                                array, np.array(patches[i])))
+                            count += 1
+                        self.assertEqual(length, count)
+
+        failure_sizes = [(-1, -1), (41, 41), (-1, 0), (0, -1),
+                         (41, 40), (40, 41), (0.1, 0.1), (0.1, 0), (0, 0.1)]
+        failure_intervals = [-1, 0.1]
+        failure_paddings = ['']
+
+        # check size
+        for size in failure_sizes:
+            for padding in paddings:
+                for interval in intervals:
+                    for image in self._test_images:
+                        gnenerator = generate_patches(
+                            image, size, interval, padding, to_image=True)
+                        while True:
+                            try:
+                                with self.assertRaises(ValueError, "invalid size %s" % str(size)):
+                                    next(gnenerator)
+                            except StopIteration:
+                                break
+                        gnenerator = generate_patches(
+                            image, size, interval, padding, to_image=True)
+                        while True:
+                            try:
+                                with self.assertRaises(ValueError, "invalid size %s" % str(size)):
+                                    next(gnenerator)
+                            except StopIteration:
+                                break
+
+        # check interval
+        for size in sizes:
+            for interval in failure_intervals:
+                for padding in paddings:
+                    for image in self._test_images:
+                        gnenerator = generate_patches(
+                            image, size, interval, padding, to_image=True)
+                        while True:
+                            try:
+                                with self.assertRaises(ValueError, "invalid interval %s" % str(interval)):
+                                    next(gnenerator)
+                            except StopIteration:
+                                break
+                        gnenerator = generate_patches(
+                            image, size, interval, padding, to_image=True)
+                        while True:
+                            try:
+                                with self.assertRaises(ValueError, "invalid interval %s" % str(interval)):
+                                    next(gnenerator)
+                            except StopIteration:
+                                break
+
+        # check padding
+        for size in sizes:
+            for interval in intervals:
+                for padding in failure_paddings:
+                    for image in self._test_images:
+                        gnenerator = generate_patches(
+                            image, size, interval, padding, to_image=True)
+                        while True:
+                            try:
+                                with self.assertRaises(ValueError, "invalid padding %s" % str(padding)):
+                                    next(gnenerator)
+                            except StopIteration:
+                                break
+                        gnenerator = generate_patches(
+                            image, size, interval, padding, to_image=True)
+                        while True:
+                            try:
+                                with self.assertRaises(ValueError, "invalid padding %s" % str(padding)):
+                                    next(gnenerator)
+                            except StopIteration:
+                                break
